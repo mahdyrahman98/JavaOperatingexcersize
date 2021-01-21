f0e6f33b14951f46f3e655c32b05a6453161d610
mahdyrahman98 <MRahman2@qa.com>
2021-01-21 10:16:19 +0000

    java excersize

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,37 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		int num1 = 6;
+		int num2 = 11;
+		int result = num1 + num2;
+		System.out.println(result);
+
+		int num3 = 32;
+		int num4 = 28;
+		int result1 = num3 * num4;
+		System.out.println(result1);
+
+		int num5 = 72;
+		int num6 = 12;
+		int result2 = num5 / num6;
+		System.out.println(result2);
+
+		int num7 = 2939;
+		int num8 = 233;
+		int result3 = num7 - num8;
+		System.out.println(result3);
+
+		int p1 = 80;
+		int b1 = 120;
+		int c1 = 100;
+		int total = p1 + b1 + c1;
+		System.out.println(total);
+		int mark = total * 100 / 450;
+		System.out.println(mark);
+
+	}
+
+}
