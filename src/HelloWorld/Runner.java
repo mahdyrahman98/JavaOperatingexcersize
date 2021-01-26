7599496104509f2c2946adb9a36e85456246c568
mahdyrahman98 <MRahman2@qa.com>
2021-01-26 10:02:33 +0000

    scanner updated

--- src/HelloWorld/Runner.java
+++ src/HelloWorld/Runner.java
@@ -1,27 +1,20 @@
 package HelloWorld;
 
-
-
 public class Runner {
 	public static void main(String[] args) {
-		Conditionals.division();
-		Conditionals.results();
-		Conditionals.flowcharta();
-		Conditionals.flowchartb(6001);
-		Conditionals.blackjack(13, 9);
-		Conditionals.uniqueSum(2, 9, 4);
-		Conditionals.tax(100000);
-
-		Array.loops();
-		Array.anotherLoop();
-		Array.stringArray();
-		Array.bigArray();
-		Array.evenOdd(8);
-		Array.arrayList();
 		
+		/*
+		 * Conditionals.division(); Conditionals.results(); Conditionals.flowcharta();
+		 * Conditionals.flowchartb(6001); Conditionals.blackjack(13, 9);
+		 * Conditionals.uniqueSum(2, 9, 4); Conditionals.tax(100000);
+		 * 
+		 * Array.loops(); Array.anotherLoop(); Array.stringArray(); Array.bigArray();
+		 * Array.evenOdd(8); Array.arrayList();
+		 */
+		 
 		Iteration.numbers(99);
 		scannerExcersize.number();
-		
+		scannerExcersize.person();
 
 	}
 
--- src/HelloWorld/scannerExcersize.java
+++ src/HelloWorld/scannerExcersize.java
@@ -1,32 +1,61 @@
 package HelloWorld;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class scannerExcersize {
-	
-	
-		public static void number() {
-	    int first, second, add, subtract, multiply;
-	    float devide;
-	    @SuppressWarnings("resource")
-		Scanner Scanner= new Scanner(System.in);
 
-	    System.out.print("Enter Two Numbers : ");
-	    first = Scanner.nextInt();
-	    second = Scanner.nextInt();
+	public static void number() {
+		int first, second, add, subtract, multiply;
+		float devide;
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
 
-	    add = first + second;
-	    subtract = first - second;
-	    multiply = first * second;
-	    devide = (float) first / second;
+		System.out.print("Enter Two Numbers : ");
 
-	    System.out.println("Sum = " + add);
-	    System.out.println("Difference = " + subtract);
-	    System.out.println("Multiplication = " + multiply);
-	    System.out.println("Division = " + devide);
-	  }
+		first = scan.nextInt();
+		second = scan.nextInt();
+
+		add = first + second;
+		subtract = first - second;
+		multiply = first * second;
+		devide = (float) first / second;
+
+		System.out.println("Sum = " + add);
+		System.out.println("Difference = " + subtract);
+		System.out.println("Multiplication = " + multiply);
+		System.out.println("Division = " + devide);
 	}
 
-	
-	
+	public static void person() {
 
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
 
+		System.out.println("Enter your name");
+		String name = scan.next();
+
+		System.out.println("Enter your age");
+		int age = scan.nextInt();
+
+		System.out.println("What is your job tite");
+		String job = scan.next();
+		job += scan.nextLine();
+
+		System.out.println(name + " " + age + " " + job);
+
+		ArrayList<Object> Information = new ArrayList<Object>();
+
+		Information.add(name);
+		Information.add(age);
+		Information.add(job);
+
+		System.out.println(Information);
+
+	}
+	
+	public static void menu () {
+		
+		
+	}
+}
